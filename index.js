eb12223a5493b84ec9ae8ae68dd849a4e161db40
Racheal Kuranchie <kuranchieracheal@gmail.com>
2022-04-21 13:40:06 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+
+const search = document.getElementById('search')
+const word = document.getElementById('word')
+const btn = document.getElementById('btn')
+const toggle = document.getElementById('icon')
+
+
+//toggling betwwen dark and light mode
+//grab the whole html content
+//if the html has a dark feature coming from styling, remove that class
+//otherwise add the class dark to it
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')){
+        html.classList.remove('dark')
+        e.target.value = "Dark mode"
+    }else{
+        html.classList.add('dark')
+        e.target.value = "Light mode"
+    }
+})
+function Check() {
+
+    //grabbing the value of the input
+    let val = search.value
+    
+    console.log(val)
+//spliting the value which is a string
+    const splited = val.split('')
+    console.log(splited)
+
+    //reverse the splited string which is now array
+    const reversed = splited.reverse()
+    console.log(reversed)
+
+    //join the string in the array after reversing
+    const joined = reversed.join('')
+    console.log(joined)
+    
+    search.value = ''
+
+    console.log(search)
+
+    if(joined === val){
+        word.innerHTML = `yieeeyy!! 🎊 🎉🎊 🎉 ${val} is a palindrom.  Well done 😍 🥰`
+    }else{
+        word.innerHTML = `sorry 😔 😔 😔 ${val} is not a palindrome, Try again 😊😊😊`
+    }
+
+    
+}
+
+btn.addEventListener('click', Check)
